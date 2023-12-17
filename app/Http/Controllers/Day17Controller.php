efeba095350d120482e8ce1b2fc7935025d26e8f
AngryMoustache <sander.vandamme11@gmail.com>
2023-12-17 10:53:26 +0100

    Day 17

---
+++ app/Http/Controllers/Day17Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class Day17Controller extends Controller
+{
+    public function one()
+    {
+        // I'm not doing Dijkstra again
+    }
+
+    public function two()
+    {
+        //
+    }
+}
